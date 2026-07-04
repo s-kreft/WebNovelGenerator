6ef9d0b39774f4b6e964acd589a7abcc3499c21c
s-kreft <sebastian.kreft.sg@gmail.com>
2026-07-04 13:34:01 +0200

    Add project files

---
+++ WebNovelGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebNovelGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
